0d2e8ca58d9a4a622a52d3a2eec01fdd2c8f2960
Mike Jin <mike@xocontinental.com>
2026-03-13 15:10:21 -0400

    pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/GW-Law-Groups",
+  assetPrefix: "/GW-Law-Groups/",
 };
 
 export default nextConfig;
